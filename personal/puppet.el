350e7d96f9c6aa2b11a746faaf569e9df0653493
Florian Zeidler <zeidler.f@googlemail.com>
2015-10-20 05:51:24 +0200

    extension for puppet and ruby development

---
+++ personal/puppet.el
@@ -0,0 +1,6 @@
+;;; package ---  Installs and configures emacs for development with Puppet
+;;; Commentary --- Try to keep it clean from Ruby-specific stuff
+;;; Code:
+(prelude-require-packages '(flymake-puppet puppet-mode puppetfile-mode))
+
+;;; End
