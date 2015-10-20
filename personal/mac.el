ff645f99129bde42593e88a9dae8fa2ace6d813a
Florian Zeidler <f.zeidler@rebuy.de>
2015-10-20 09:11:42 +0200

    Use command as meta, to type everything again.

---
+++ personal/mac.el
@@ -0,0 +1,7 @@
+;;; Mac --- Mac specific settings
+;;; Helps to adapt to the mac.
+;;; Code:
+(setq default-input-method "MacOSX")
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'none)
+;;; End
