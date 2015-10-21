e254e26f036bacc72301e5e55a7fb9b8009683d4
Florian Zeidler <f.zeidler@rebuy.de>
2015-10-21 14:38:23 +0200

    remove troublesome package

--- personal/ruby.el
+++ personal/ruby.el
@@ -1,7 +1,7 @@
 ;;; Ruby --- Install and configure dependencies for Ruby development
 ;;; Commentary --- Keep Puppet out
 ;;; Code:
-(prelude-require-packages '(flymake-ruby helm-rb ruby-additional ruby-block))
+(prelude-require-packages '(flymake-ruby helm-rb ruby-additional))
 (prelude-require-packages '(ruby-electric ruby-end ruby-refactor ruby-test-mode))
 (prelude-require-packages '(ruby-tools rspec-mode))
 ;;; End
