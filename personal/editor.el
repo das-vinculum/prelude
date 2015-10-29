07bc0d8a13bacef3caa004dd82af2fc338f7eec7
Florian Zeidler <f.zeidler@rebuy.de>
2015-10-29 15:20:04 +0100

    add org mode, and tmux alike splitt

--- personal/editor.el
+++ personal/editor.el
@@ -6,4 +6,8 @@
 (global-linum-mode 1)
 (linum-relative-mode 1)
 (setq backup-directory-alist '(("" . "~/tmp/emacs-backup")))
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+(global-set-key (kbd "C-c |") 'split-window-horizontally)
+(global-set-key (kbd "C-c -") 'split-window-vertically)
 ;;; End;
---
+++ personal/org.el
@@ -0,0 +1,1 @@
+(setq org-cycle-agenda-files (list "~/Google Drive/org"))
